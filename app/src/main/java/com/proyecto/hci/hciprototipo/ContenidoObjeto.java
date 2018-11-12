93df8ee9909427a95829751d95a09746734aa420
HuskyDM <javialofer@hotmail.com>
2018-11-12 17:59:58 -0600

    Listo con esta parte

--- app/src/main/java/com/proyecto/hci/hciprototipo/ContenidoObjeto.java
+++ app/src/main/java/com/proyecto/hci/hciprototipo/ContenidoObjeto.java
@@ -2,6 +2,9 @@ package com.proyecto.hci.hciprototipo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ContenidoObjeto extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class ContenidoObjeto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenido_objeto);
+
+    ImageButton btn_ret = findViewById(R.id.btn_return);
+
+    btn_ret.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    });
     }
 }
