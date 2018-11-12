ab3359fc05e0d8c677bfd7c26728d1b0fbc2347e
HuskyDM <javialofer@hotmail.com>
2018-11-12 17:54:13 -0600

    Listo con esta parte

--- app/src/main/java/com/proyecto/hci/hciprototipo/ContenidoHistorial.java
+++ app/src/main/java/com/proyecto/hci/hciprototipo/ContenidoHistorial.java
@@ -2,6 +2,9 @@ package com.proyecto.hci.hciprototipo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ContenidoHistorial extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class ContenidoHistorial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenido_historial);
+
+        ImageButton btn_ret = findViewById(R.id.btn_ret);
+
+        btn_ret.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
